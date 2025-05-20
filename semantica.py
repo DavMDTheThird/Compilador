f93cabb99bd178ad4d8ec73c9245c797a6129e54
Dav <dm39988@gmail.com>
2025-05-19 19:40:02 -0600

    [ADD][V_2.68] Added a better way to go through the AST, created some functions, and added parameters as elements of the list

--- semantica.py
+++ semantica.py
@@ -36,34 +36,42 @@ def globales(prog, pos, long, line_ = 0, symbol_tables_ = []):
     globalesParser(programa, posicion, progLong)
 
 
+# Elemento de una declaracion -------------------------------------------------------------------
+class DecElement:
+    def __init__(self, symbol, type, arr=None):
+        self.symbol = symbol
+        self.type = type
+        self.arr = arr
+
+    def __str__(self):
+        # Print as "type symbol[arr]" if arr exists, else "type symbol"
+        arr_str = self.arr if self.arr else ""
+        return f"{self.type} AAAA{self.symbol}{arr_str}"
+
 # Elemento de la tabla de simbolos -------------------------------------------------------------------
 class TableElement:
-    def __init__(self, symbol, type_=None, line=None, param=None, size=None, return_=None):
+    def __init__(self, symbol, type=None, line=None, param=None, size=None, returnE=None):
         self.symbol = symbol
         self.line = line
-        self.type = type_
+        self.type = type
         self.param = param
         self.size = size
-        self.return_ = return_
+        self.returnE = returnE
 
     def __str__(self):
         def fmt(val):
             if isinstance(val, list):
-                # Format parameters as "type name"
-                return ", ".join(f"{t} {n}" for t, n in val)
+                # Use DecElement's __str__ for each parameter
+                return ", ".join(str(p) for p in val)
             return "----" if val is None else str(val)
         return (f"{fmt(self.symbol):<10} | {fmt(self.type):<8} | {fmt(self.line):<5} | "
-                f"{fmt(self.param):<30} | {fmt(self.size):<5} | {fmt(self.return_):<10}")
+                f"{fmt(self.param):<30} | {fmt(self.size):<5} | {fmt(self.returnE):<10}")
 
 # Tabla de simbolos ----------------------------------------------------------------------------------
 class SymbolTable:
     def __init__(self, name="Global"):
         self.name = name
         self.elements = []  # List of TableElement objects
-
-    def add_symbol(self, symbol, line=None, type_=None, param=None, size=None, return_=None):
-        elem = TableElement(symbol, line, type_, param, size, return_)
-        self.elements.append(elem)
 
     # def get_symbol(self, symbol):
     #     for elem in self.elements:
@@ -79,101 +87,158 @@ class SymbolTable:
             output += str(elem) + "\n"
         return output
 
-def debug_print(node):
-    match = next((x for x in node.children if x.symbol == PT.type_specifier), None)
-    print(f"UNUNUNUNUNUNUNUNUNUNUNUNUNUNNUN:", end=" ")
-    for child in node.children[:-1]:
-        print(f"{child.symbol}", end=", ")
-    print(f"{node.children[-1].symbol}")
+def debug_print(node, lst = []):
+    nodetest = node
+    if lst:
+        for i in lst:
+            nodetest = next((x for x in nodetest.children if x.symbol == i), None)
 
+    if not nodetest: 
+        print("ERROR: not found")
+        nodetest = node
+
+    print(f"UNUNUNUNUNUNUNUNUNUNUNUNUNUNNUN:", end=" ")
+    for child in nodetest.children:
+        print(f"{child.symbol}", end=", ")
+    print("")
+
+def get_node(node, lst = []):
+    nodetest = node
+    if lst:
+        for i in lst:
+            if nodetest:
+                nodetest = next((x for x in nodetest.children if x.symbol == i), None)
+            else:
+                nodetest = None
+                break
+
+    if not nodetest: 
+        # print(f"ERROR: not found while searching {lst}")
+        pass
+
+    return nodetest
 
 def get_VarParam_info(node):
     type = None
     value = None
+    array = ""
 
-    match = next((x for x in node.children if x.symbol == PT.type_specifier), None)
+    match = get_node(node, [PT.type_specifier])
     # Esta recursion le agrega seguridad de que no le vaya a llegar un nodo sin hijos
     if match and match.children:
         type = match.children[0].symbol
 
-    match = next((x for x in node.children if x.symbol == TokenType.ID), None)
+    match = get_node(node, [TokenType.ID])
     if match and match.children:
         value = match.children[0].symbol
+        
+        arr_val = is_array(node)
+        if arr_val:
+            print("GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG")
+            array = "[" + arr_val + "]"
 
-    return type, value
+    return type, value + array
 
+def getFuncitondEnd(node):
+    return get_node(node, [PT.dec_p, PT.compound_stmt, "}"])            
 
 def is_function(node):
     param_lst = []
 
-    match = next((x for x in node.children if x.symbol == PT.dec_p), None)
+    match = get_node(node, [PT.dec_p])
     # Si la declaracion es una funcion:
-    if next((x for x in match.children if x.symbol == "("), None):
+    if get_node(match, ["("]):
         # Los parametros de la funcion:
-        match = next((x for x in match.children if x.symbol == PT.params), None)
+        match = get_node(match, [PT.params, PT.param_list])
         if match:
+            param = get_node(match, [PT.param])
+            if param:
+                # debug_print(param)
 
-            match = next((x for x in match.children if x.symbol == PT.param_list), None)
-            if match:
+                arr_val = is_array(param)
+                param_type, param_val = get_VarParam_info(param)
 
-                param = next((x for x in match.children if x.symbol == PT.param), None)
+                param_lst.append(DecElement(param_val, param_type, arr_val))
+
+            # Itera por todas las N cantidad de parametros
+            match = get_node(match, [PT.param_list_p])
+            while match:
+
+                param = get_node(match, [PT.param])
                 if param:
+                    arr_val = is_array(param)
                     param_type, param_val = get_VarParam_info(param)
-                    param_lst.append([param_type, param_val])
+                    param_lst.append(DecElement(param_val, param_type, arr_val))
 
-                # Itera por todas las N cantidad de parametros
-                match = next((x for x in match.children if x.symbol == PT.param_list_p), None)
-                while match:
+                match = get_node(match, [PT.param_list_p])
 
-                    param = next((x for x in match.children if x.symbol == PT.param), None)
-                    if param:
-                        param_type, param_val = get_VarParam_info(param)
-                        param_lst.append([param_type, param_val])
-
-                    match = next((x for x in match.children if x.symbol == PT.param_list_p), None)
-
-                return True, param_lst
-            else:
-                return True, param_lst    
+            return True, param_lst
+        else:
+            return True, param_lst    
     else:
         return False, param_lst
+    
+def is_array(node):
+    print(f"Tryinggggggggggggggg ARAYYYYYYYYYYYYY")
+
+    func_arr = get_node(node, [PT.param_p, "["])
+    alone_arr = get_node(node, [PT.dec_p, "["])
+
+    if func_arr:
+        print(f"I've fouund and ARRAYYYYYYY: {0}")
+        return 0
+
+    elif alone_arr:
+        my_node = get_node(node, [PT.dec_p, TokenType.NUM])
+        print(f"I've fouund and ARRAYYYYYYY: {my_node.children[0].symbol}")
+        return my_node.children[0].symbol
+    
+    return None
+
 
 
 def pre_order(node, declaration = False, new_table = None, endTableElement = None):
+    nextChildren = node
 
     if node is None:
         return
 
     if declaration == False and node.symbol == PT.dec:
-
-        new_table = SymbolTable()
-
-        declaration = True
         print(f"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA: {node.symbol}")
+        declaration = True
 
         node_type, node_val =  get_VarParam_info(node)
-        a, b = is_function(node)
-        if a:
-            new_table.elements.append(TableElement(node_val, node_type, None, b, None, node_type))
+
+        new_table = SymbolTable(node_val)
+
+        func, func_params = is_function(node)
+        if func:
+            new_table.elements.append(TableElement(node_val, node_type, None, func_params, None, node_type))
             print(new_table)
-            # print(f"I've found a function with {len(b)} parameters: ")
-            # if b:
-            #     for i in b:
-            #         print(i)
-            test = next((x for x in node.children if x.symbol == PT.dec_p), None)
-            debug_print(test)
-            test2 = next((x for x in test.children if x.symbol == PT.compound_stmt), None)
-            debug_print(test2)
-            test3 = next((x for x in test2.children if x.symbol == "}"), None)
-            print(f"FOUNDDDDDDDDDDDDDDDDDDDDDDDDD_____: {test3.symbol}")
-            endTableElement = test3
+            
+            # Stop iterating over the function declaration, and continue to its <compund statement>
+            endTableElement = getFuncitondEnd(node)
+            match = get_node(node, [PT.dec_p, PT.compound_stmt])
+            if match:
+                nextChildren = match
+
+            print(f"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB: {node.symbol}")
+            for element in func_params:
+                print(element)
+                new_table.elements.append(TableElement(element.symbol, 
+                                                       element.type, None, None, None, None))
 
 
     if declaration:
         if node == endTableElement:
-            print(f"MATCHHHHHHHHHHHHHHHHHHHHHHHH_____: {node.symbol}")
             symbol_tables.append(new_table)
+            print(f"YES: {node.symbol}")
+        
         else:
+            if node.symbol == PT.dec:
+                print(f"FOUNDDDDDDDDDDDDDDD NEWWWWWWWWWWW: {node.symbol}")
+
+
             print(f"YES: {node.symbol}")
 
     else:
@@ -182,11 +247,11 @@ def pre_order(node, declaration = False, new_table = None, endTableElement = Non
 
     
     # Traverse children
-    for child in node.children:
+    for child in nextChildren.children:
         pre_order(child, declaration, new_table, endTableElement)
 
-    # if not node.children:
-    #     print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    if not node.children:
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
 
 # --------------------------------------------------------------------------------------------------------------
@@ -196,7 +261,6 @@ def tabla(tree_root, imprime=True):
     global symbol_tables
 
     pre_order(tree_root[0])
-
     
     if imprime:
         print("\nSymbol Tables:")
